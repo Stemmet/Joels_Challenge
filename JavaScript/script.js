1336d901c1727b37ec9409bba6d8194b62e916e4
Stemmet <cameronstemmet2@gmail.com>
2022-06-07 10:29:49 +0200

    '

---
+++ JavaScript/script.js
@@ -0,0 +1,9 @@
+const bestFriend = [
+    'Cameron','Mikhail', 'Jesse', 'Muda', 'King', 'Cassidy', 'Amanda', 'Joshe', 'Reagon', 'Xena'
+]
+
+
+console.log(bestFriend.length/2-1);
+
+console.log(bestFriend[bestFriend.length/2-1])
+console.log(bestFriend[bestFriend.length-1])
